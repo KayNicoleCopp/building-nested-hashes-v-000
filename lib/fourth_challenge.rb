70c7c2ec9be77ef96a44fcbf43f817b81d033640
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-13 15:50:17 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,8 +8,8 @@ def fourth_challenge
         :name => "Benvolio",
         :age => "17",
         :attitude => "worried",
-        :name => "Mercutio"
-        :age => "18"
+        :name => "Mercutio",
+        :age => "18",
         :attitude => "hot-headed"
       ]
    },
@@ -21,8 +21,8 @@ def fourth_challenge
         :name => "Steven",
         :age => "30",
         :attitude => "confused",
-        :name => "Nurse"
-        :age => "44"
+        :name => "Nurse",
+        :age => "44",
         :attitude => "worried"
       ]
    }
